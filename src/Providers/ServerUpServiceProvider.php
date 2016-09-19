f7c690e3de099b52d8449ee190ced8ada0345a17
Kaan Kilic <kaankilic@Kaan-MacBook-Pro.local>
2016-09-20 01:09:49 +0300

    configuration file implemented

--- src/Providers/ServerUpServiceProvider.php
+++ src/Providers/ServerUpServiceProvider.php
@@ -12,7 +12,7 @@ class ServerUpServiceProvider extends ServiceProvider {
   public function boot(\Illuminate\Routing\Router $router){
    $this->app->bind('ServerUp', 'Kaankilic\ServerUp\Libraries\ServerUp' );
    $this->publishes([
-      __DIR__.'/../../config/serverup.php' => config_path('serverup.php')
+      __DIR__.'/../config/serverup.php' => config_path('serverup.php')
     ],'config');
   }
  
@@ -22,7 +22,7 @@ class ServerUpServiceProvider extends ServiceProvider {
     * @return void
   */
   public function register(){
-    $this->mergeConfigFrom(__DIR__ . '/../../config/serverup.php', 'serverup');
+    $this->mergeConfigFrom(__DIR__ . '/../config/serverup.php', 'serverup');
     return array('ServerUp');
   }
 }
